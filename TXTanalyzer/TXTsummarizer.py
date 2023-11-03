69109114b4de38a4a84e74c784e4bd48796facd9
maialenaguiriano <83809739+maialenaguiriano@users.noreply.github.com>
2023-11-03 13:16:05 +0100

    Creación clases

--- TXTanalyzer/TXTsummarizer.py
+++ TXTanalyzer/TXTsummarizer.py
@@ -3,91 +3,19 @@ from nltk import sent_tokenize, word_tokenize, PorterStemmer
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
 from wordcloud import WordCloud
+from TXTanalyzer import TXTanalyzer
 
-"""
-
-Text analysis package.
-It is a package generated to perform easy analysis on .txt files. 
-It makes it easy to analyze texts through different tools and facilities like word counting, sentence splitting, sentiment analysis and so on.
-Different packages like spicy or nltk have been used to facilitate the development of the tool and increase the output quality.
-
-"""
-
-class TXTanalyzer():
-
-    def __init__(self, txt_file_path:str):
-        self.text = self.read_file(txt_file_path)
-        if self.text == "":
-            raise Exception("El fichero no puede estar vacío. Debe contener algún tipo de texto.")
-
-    def read_file(self, txt_file_path:str):
-        self.txt_file_path = txt_file_path
-        if txt_file_path.lower().endswith('.txt'):
-            file = open(txt_file_path,"r")
-            return file.read()
-        else:
-            raise Exception("El fichero debe tener extensión .txt.")
-        
-    def print_text(self):
-        return self.text
-    
-    def sentence_splitting(self):
-        try:
-            self.nlp = spacy.load("en_core_web_sm")
-        except OSError:
-            # Model not found, download it
-            print(f"Descargando modelo de separación de frases...")
-            spacy.cli.download("en_core_web_sm")
-            self.nlp = spacy.load("en_core_web_sm")
-        finally:
-            frases = self.nlp(self.text)
-        return [sent.text for sent in frases.sents]
-    
-    def __remove_stopwords(self):
-        cleaned_text = self.text.translate(str.maketrans('', '', string.punctuation))
-        all_words = cleaned_text.split()
-        stop_words = set(stopwords.words('spanish'))
-        filtered_words = [word.replace('¿', '') for word in all_words if word.lower() not in stop_words] # el carácter '¿' no lo detecta la librería string por lo que aprovechamos para eliminarlo ahora
-        return filtered_words
-    
-    def word_count(self):
-        """
-        Must consider that stopwords are previously removed.
-        """
-        try:
-            self.words = self.__remove_stopwords()
-        except OSError:
-            # Model not found, download it
-            print(f"Descargando modelo para eliminar stopwords...")
-            nltk.download('stopwords')
-            self.words = self.__remove_stopwords()
-        total_chars = [char for char in self.text]
-        return f"Word count: {len(self.words)}\nUnique word count: {len(set(self.words))}\nTotal characters: {len(total_chars)}"
-    
-    def word_freq(self):
-        self.word_count()
-        word_frequency = {}
-        for word in self.words:
-            word = word.lower()
-            word_frequency[word] = word_frequency.get(word, 0) + 1
-        return word_frequency
-    
-    def wordcloud(self):
-        wordcloud = WordCloud().generate_from_frequencies(self.word_freq())
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis("off")
-        return plt.show()
-    
-    def sia(self):
-        """
-        SIA stands for Sentiment Intensity Analyzer
-        """
-        sia = SentimentIntensityAnalyzer().polarity_scores(self.text)
-        neg, neu, pos = sia['neg'], sia['neu'], sia['pos']
-        return f"Negativity: {neg}\nNeutrality: {neu}\nPositivity: {pos}"
-    
 class TXTsummarizer(TXTanalyzer):
     def __init__(self, txt_file_path:str):
+        """
+        Constructor de la clase. Realiza el procesamiento y generación de un resumen a partir de un archivo de texto.
+
+        Args:
+            txt_file_path (str): La ruta al archivo de texto a procesar y resumir.
+
+        Raises:
+            Exception: Se lanza una excepción si el archivo está vacío.
+        """
         super().__init__(txt_file_path)
         self.text = self.read_file(txt_file_path)
         if self.text == "":
@@ -98,11 +26,11 @@ class TXTsummarizer(TXTanalyzer):
         self.sentences = sent_tokenize(self.summ)
         self.total_documents = len(self.sentences)
         # 2. Crear matriz de frecuencias
-        self.freq_matrix = self.__create_frequency_matrix() #divide por palabras cada frase y calcula la frecuencia en la que aparecen POR FRASE
+        self.freq_matrix = self.__create_frequency_matrix()
         # 3. Calcular TermFrequency y generar matriz
-        self.tf_matrix = self.__create_tf_matrix() #probabilidad por la que se repite cada palabra en cada frase
+        self.tf_matrix = self.__create_tf_matrix()
         # 4. Porcentaje de aparicion
-        self.count_doc_per_words = self.__create_documents_per_words() #veces que aparece cada palabra en el texto completo
+        self.count_doc_per_words = self.__create_documents_per_words()
         # 5. Calcular IDF y generar matriz
         self.idf_matrix = self.__create_idf_matrix()
         # 6. Calcular TF*IDF y generar matriz
@@ -113,15 +41,29 @@ class TXTsummarizer(TXTanalyzer):
         self.threshold = self.__find_average_score()
         # 9. Generar resumen
         self.summary = self.__generate_summary()
+        # Guardar resumen en la variable text para aplicar las funciones de TXTanalyzer
         self.text = self.summary
 
     def __procesar_texto(self):
+        """
+        Procesa el texto eliminando saltos de línea, signos de interrogación, signos de exclamación y viñetas,
+        y realiza una limpieza adicional eliminando espacios duplicados.
+
+        Returns:
+            str: El texto procesado.
+        """
         summ = self.text.replace("\n", " ").replace("¿","").replace("?",'.')
         summ = summ.replace("•", "")
         summ = " ".join(summ.split())
         return summ
     
     def __create_frequency_matrix(self):
+        """
+        Crea una matriz de frecuencia que asocia las palabras en las oraciones del texto con su frecuencia de aparición.
+
+        Returns:
+            dict: Un diccionario que contiene las frecuencias de las palabras en las oraciones del texto.
+        """
         frequency_matrix = {}
         stopWords = set(stopwords.words("spanish"))
         ps = PorterStemmer()
@@ -141,6 +83,12 @@ class TXTsummarizer(TXTanalyzer):
         return frequency_matrix
     
     def __create_tf_matrix(self):
+        """
+        Crea una matriz TF (Frecuencia de Términos) que representa la frecuencia relativa de las palabras en las oraciones.
+
+        Returns:
+            dict: Un diccionario que contiene la matriz TF del texto.
+        """
         tf_matrix = {}
         for sent, f_table in self.freq_matrix.items():
             tf_table = {}
@@ -152,6 +100,12 @@ class TXTsummarizer(TXTanalyzer):
         return tf_matrix
     
     def __create_documents_per_words(self):
+        """
+        Crea una tabla que asocia las palabras con la cantidad de documentos en los que aparecen.
+
+        Returns:
+            dict: Un diccionario que contiene la cantidad de documentos en los que aparece cada palabra.
+        """
         word_per_doc_table = {}
         for sent, f_table in self.freq_matrix.items():
             for word, count in f_table.items():
@@ -162,6 +116,12 @@ class TXTsummarizer(TXTanalyzer):
         return word_per_doc_table
     
     def __create_idf_matrix(self):
+        """
+        Crea una matriz IDF (Frecuencia Inversa de Documentos) que representa el valor IDF de cada palabra en el texto.
+
+        Returns:
+            dict: Un diccionario que contiene la matriz IDF del texto.
+        """
         idf_matrix = {}
         for sent, f_table in self.freq_matrix.items():
             idf_table = {}
@@ -171,6 +131,13 @@ class TXTsummarizer(TXTanalyzer):
         return idf_matrix
 
     def __create_tf_idf_matrix(self):
+        """
+        Crea una matriz TF-IDF que representa el producto de la matriz TF (Frecuencia de Términos) y la matriz IDF
+        (Frecuencia Inversa de Documentos) para cada palabra en el texto.
+
+        Returns:
+            dict: Un diccionario que contiene la matriz TF-IDF del texto.
+        """
         tf_idf_matrix = {}
         for (sent1, f_table1), (sent2, f_table2) in zip(self.tf_matrix.items(), self.idf_matrix.items()):
             tf_idf_table = {}
@@ -179,11 +146,13 @@ class TXTsummarizer(TXTanalyzer):
             tf_idf_matrix[sent1] = tf_idf_table
         return tf_idf_matrix
 
-    def __score_sentences(self) -> dict:
+    def __score_sentences(self):
         """
-        score a sentence by its word's TF
-        Basic algorithm: adding the TF frequency of every non-stop word in a sentence divided by total no of words in a sentence.
-        :rtype: dict
+        Evalúa la puntuación de cada oración en función de la TF-IDF de sus palabras. El algoritmo básico consiste en sumar la frecuencia TF-IDF de cada palabra no vacía en una oración y
+        dividirla por el número total de palabras en la oración.
+
+        Returns:
+            dict: Un diccionario que asocia cada oración con su puntuación.
         """
         sentenceValue = {}
         for sent, f_table in self.tf_idf_matrix.items():
@@ -194,10 +163,12 @@ class TXTsummarizer(TXTanalyzer):
             sentenceValue[sent] = total_score_per_sentence / count_words_in_sentence
         return sentenceValue
     
-    def __find_average_score(self) -> int:
+    def __find_average_score(self):
         """
-        Find the average score from the sentence value dictionary
-        :rtype: int
+        Calcula el puntaje promedio a partir del diccionario de valores de las oraciones.
+
+        Returns:
+            int: El puntaje promedio de las oraciones.
         """
         sumValues = 0
         for entry in self.sentenceValue:
@@ -207,6 +178,12 @@ class TXTsummarizer(TXTanalyzer):
         return average
     
     def __generate_summary(self):
+        """
+        Genera un resumen a partir de las oraciones del texto en función de sus puntuaciones.
+
+        Returns:
+            str: Un resumen generado a partir de las oraciones seleccionadas.
+        """
         sentence_count = 0
         summary = ''
         for sentence in self.sentences:
@@ -214,9 +191,3 @@ class TXTsummarizer(TXTanalyzer):
                 summary += " " + sentence
                 sentence_count += 1
         return summary
-
-if __name__ == "__main__":
-    # analyzer = TXTanalyzer('tiktok.txt')
-    # print(analyzer.sia())
-    summarizer = TXTsummarizer('tiktok.txt')
-    print(summarizer.sia())
